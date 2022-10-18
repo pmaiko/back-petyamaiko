b2251824ae4469ffdc596707522f3bcdacf97cdc
petyamaiko <petyamaiko@gmail.com>
2022-10-18 23:07:41 +0300

    google drive

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class ImageController extends Controller {
   public function post () {
     Storage::disk('google')->put('test.txt', 'Hello World');
-    Storage::disk('local')->put('example.txt', 'Contents');
+//    Storage::disk('local')->put('example.txt', 'Contents');
     echo 1;
   }
 }
